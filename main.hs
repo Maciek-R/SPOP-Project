b61a9fdb6386ea65f146a8f6aa8971a5dabf78a5
Maciej Ruszczyk <m.ruszczyk.1@stud.elka.pw.edu.pl>
2019-01-05 15:33:35 +0100

    Added exiting from application and checking if puzzle's number is correct

--- main.hs
+++ main.hs
@@ -1,4 +1,5 @@
 import System.IO
+import System.Exit
 
 printWords [] = return ()
 printWords (x:xs) = do
@@ -29,8 +30,7 @@ crossLetterOnBoard (row:rows) x y | y==0 = (crossLetterFromLine row x):(crossLet
 								  
 exampleCrossedBoard board = 
 	(crossLetterOnBoard (crossLetterOnBoard (crossLetterOnBoard board 3 0) 2 0) 1 0)
-						
-			
+							
 boardFileName nr |  nr=="1" = "board_1.txt"
 				 |  nr=="2" = "board_2.txt"
 				 |  nr=="3" = "board_3.txt"
@@ -38,27 +38,37 @@ boardFileName nr |  nr=="1" = "board_1.txt"
 wordsFileName nr |  nr=="1" = "words_1.txt"
 				 |  nr=="2" = "words_2.txt"
 				 |  nr=="3" = "words_3.txt"
-			
+	
+mainPuzzle nr = do
+				boardIO <- readFile (boardFileName nr) 
+				wordsIO <- readFile (wordsFileName nr) 
+				--checking files TODO
+				--algorithm TODO
+				let board = lines boardIO
+				let words = lines wordsIO
+				let usedWords = [nthElem words 0, nthElem words 5, nthElem words 7]
+				printBoards board (rowsWithoutUsedWords board usedWords)
+				putStrLn ""
+				putStrLn "All words:"
+				printWords words
+				putStrLn ""
+				putStrLn "Used words"
+				printWords usedWords
+				putStrLn ""
+				putStrLn "Example:"
+				printBoards (exampleCrossedBoard board) board
+		
 main =	do
 		putStrLn "1. Puzzle1"
 		putStrLn "2. Puzzle2"
 		putStrLn "3. Puzzle3"
+		putStrLn "0. Wyjscie"
 		putStrLn "Wybierz zagadke: "
 		nr <- getLine
-		boardIO <- readFile (boardFileName nr) 
-		wordsIO <- readFile (wordsFileName nr) 
-		--checking files TODO
-		--algorithm TODO
-		let board = lines boardIO
-		let words = lines wordsIO
-		let usedWords = [nthElem words 0, nthElem words 5, nthElem words 7]
-		printBoards board (rowsWithoutUsedWords board usedWords)
-		putStrLn ""
-		putStrLn "All words:"
-		printWords words
-		putStrLn ""
-		putStrLn "Used words"
-		printWords usedWords
-		putStrLn ""
-		putStrLn "Example:"
-		printBoards (exampleCrossedBoard board) board
+		if nr == "0" 
+			then return ()
+		else if not (nr == "1" || nr == "2" || nr == "3")
+			then main
+		else 
+			mainPuzzle nr
+				
